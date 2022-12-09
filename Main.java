cb449cd4f4ecdb89a08a8b727fe07ef20f5b555e
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2022-12-09 12:50:05 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -30,45 +30,78 @@ public class operation {
         result = a - b;
         System.out.println(a + " - "+ b +" = " + result);
         break;
-      //multiplication
-      case '*' :
-        result = a * b;
-        System.out.println(a + " * "+ b +" = " + result);
+import java.util.Scanner;
+import java.io.*;
+
+public class operation {
+
+  double a;
+  double b;
+  char operator;
+  double result;
+
+
+  // parameterised constructer
+  operation(int a, int b, char operator) {
+    this.a = a;
+    this.b = b;
+    this.operator = operator;
+  }
+
+  public void display() {
+    switch (operator) {
+      // addition
+      case '+':
+        result = a + b;
+        System.out.println(a + " + " + b + " = " + result);
         break;
-      //division with exception handling
-      case '/' :
-        try{
+      // substractiom
+      case '-':
+        result = a - b;
+        System.out.println(a + " - " + b + " = " + result);
+        break;
+      // multiplication
+      case '*':
+        result = a * b;
+        System.out.println(a + " * " + b + " = " + result);
+        break;
+      // division with exception handling
+      case '/':
+        try {
           result = a / b;
-        }
+        } 
         catch (ArithmeticException e) {
           System.out.println("DIVISION BY ZERO NOT POSSIBLE");
         }
-        System.out.println(a + " / "+ b +" = " + result);
+        System.out.println(a + " / " + b + " = " + result);
         break;
-      //invalid operator
-      default :
+      // invalid operator
+      default:
         System.out.println("Input a valid operator -_-");
         break;
+
+    }
   }
 }
+public class main{
 
-public class Main {
-    
-  // create an object of Scanner class
-  Scanner input = new Scanner(System.in);
-  
   public static void main(String[] args) {
 
+    // object
+    operation op = new operation(double a, double b, char operator);
+    // create an object of Scanner class
+    Scanner input = new Scanner(System.in);
+    
     // ask users to enter operator
     System.out.println("Choose an operator: +, -, *, or /");
-    operator = input.next().charAt(0);
+    char operator = input.next().charAt(0);
 
     // ask users to enter numbers
     System.out.println("Enter first number");
-    a = input.nextDouble();
+    double a = input.nextDouble();
 
     System.out.println("Enter second number");
-    b = input.nextDouble();
+    double b = input.nextDouble();
 
     input.close();
 
