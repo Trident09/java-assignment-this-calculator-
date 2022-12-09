74d79e01ab552fc17519e43aeea385948ac39032
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2022-12-09 15:17:56 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,110 +1,78 @@
-import java.util.*;
-import java.io.*;
-
-public class operation {
-  
-  double a;
-  double b;
-  char operator;
-  double result;
-
-  //object
-  operation op = new operation();
-  
-  //parameterised constructer
-  operation(int a, int b, char operator){
-    this.a = a;
-    this.b = b;
-    this.operator = operator;
-  }
-
-  public void display(){
-    switch (operator){
-      //addition
-      case '+' :
-        result = a + b;
-        System.out.println(a + " + "+ b +" = " + result);
-        break;
-      //substractiom
-      case '-' :
-        result = a - b;
-        System.out.println(a + " - "+ b +" = " + result);
-        break;
 import java.util.Scanner;
-import java.io.*;
 
-public class operation {
+class calculator
+{
 
-  double a;
-  double b;
-  char operator;
-  double result;
+  public double a;
+  public double b;
+  public char operator;
+  public double result;
 
-
-  // parameterised constructer
-  operation(int a, int b, char operator) {
+  calculator(double a, double b)
+  {
     this.a = a;
     this.b = b;
-    this.operator = operator;
   }
 
-  public void display() {
-    switch (operator) {
-      // addition
-      case '+':
-        result = a + b;
-        System.out.println(a + " + " + b + " = " + result);
-        break;
-      // substractiom
-      case '-':
-        result = a - b;
-        System.out.println(a + " - " + b + " = " + result);
-        break;
-      // multiplication
-      case '*':
-        result = a * b;
-        System.out.println(a + " * " + b + " = " + result);
-        break;
-      // division with exception handling
-      case '/':
-        try {
-          result = a / b;
-        } 
-        catch (ArithmeticException e) {
-          System.out.println("DIVISION BY ZERO NOT POSSIBLE");
-        }
-        System.out.println(a + " / " + b + " = " + result);
-        break;
-      // invalid operator
-      default:
-        System.out.println("Input a valid operator -_-");
-        break;
-
-    }
+  void addition()
+  {
+    result = (a + b);
+    System.out.println("Ans = "+result+"/n");
   }
-}
-public class main{
+  void substraction()
+  {
+    result = (a - b);
+    System.out.println("Ans = "+result+"/n");
+  }
+  void multiplication()
+  {
+    result = (a * b);
+    System.out.println("Ans = "+result+"/n");
+  }
+  void division()
+  {
+    result = (a / b);
+    System.out.println("Ans = "+result+"/n");
+  }
+  void modulo()
+  {
+    result = (a % b);
+    System.out.println("Ans = "+result+"/n");
+  }
 
-  public static void main(String[] args) {
-
-    // object
-    operation op = new operation(double a, double b, char operator);
-    // create an object of Scanner class
+  public static void main(String[] args)
+  {
     Scanner input = new Scanner(System.in);
-    
-    // ask users to enter operator
-    System.out.println("Choose an operator: +, -, *, or /");
+    System.out.println("Enter two numbers ^-^");
+    double a = input.nextdouble();
+    double b = input.nextdouble();
     char operator = input.next().charAt(0);
+    calculator op = new calculator(double a, double b);
+    switch (operator)
+      {
+        case '+' :
+          op.addition();
+          break;
 
-    // ask users to enter numbers
-    System.out.println("Enter first number");
-    double a = input.nextDouble();
+        case '-' :
+          op.substraction();
+          break;
 
-    System.out.println("Enter second number");
-    double b = input.nextDouble();
+        case '*' :
+          op.multiplication();
+          break;
 
-    input.close();
+        case '/' :
+          op.division();
+          break;
 
-    op.display();
+        case '%' :
+          op.modulo();
+          break;
+
+        default :
+          System.out.println("Enter valid operator -_-");
+          break;
+      }
   }
 }
